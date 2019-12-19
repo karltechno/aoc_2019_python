b23ebdbd26893e80f74046083541734536c0b864
Karl Davis <karl@karltechno.com>
2019-12-19 14:45:19 +0000

    Fix day9, param read now outputs addresses for output fns

--- day9.py
+++ day9.py
@@ -24,16 +24,6 @@ class interpreter:
     def _write_output(self, output):
         self.output.append(output)
 
-    def _get_val(self, val, mode):
-        if mode == 0:
-            return self._read_mem(val)
-        elif mode == 1:
-            return val
-        elif mode == 2:
-            return self._read_mem(val + self.relative_base)
-        
-        raise ValueError(f"Bad addressing mode {mode}")
-
     def _ensure_mem_addr(self, addr):
         if addr < 0:
             raise ValueError(f"Address ({addr}) must be positive")
@@ -48,22 +38,20 @@ class interpreter:
         self._ensure_mem_addr(addr)
         return self.mem[addr]
 
+    def _get_param_addrs(self, addr_mode, count):
+        d = { 0 : lambda x : self._read_mem(x), 1 : lambda x : x, 2 : lambda x : self._read_mem(x) + self.relative_base }
+        l = [ d[addr_mode[i]](self.pc + i) for i in range(count) ]
+        self.pc += count
+        return l
+
     def _binary_op(self, fn, instr_modes):
-        src0 = self._read_mem(self.pc + 0)
-        src1 = self._read_mem(self.pc + 1)
-        dest = self._read_mem(self.pc + 2)
-        self.pc += 3
-        self._write_mem(dest, fn(self._get_val(src0, instr_modes[0]), self._get_val(src1, instr_modes[1])))
+        params = self._get_param_addrs(instr_modes, 3)
+        self._write_mem(params[2], fn(self._read_mem(params[0]), self._read_mem(params[1])))
 
     def _jump_op(self, test, instr_modes):
-        src0 = self._read_mem(self.pc + 0)
-        src1 = self._read_mem(self.pc + 1)
-        p0 = self._get_val(src0, instr_modes[0])
-        p1 = self._get_val(src1, instr_modes[1])
-        if test(p0):
-            self.pc = p1
-        else:
-            self.pc += 2
+        params = [self._read_mem(x) for x in self._get_param_addrs(instr_modes, 2)]
+        if test(params[0]):
+            self.pc = params[1]
 
     def run_with_input(self, input):
         if self.halted:
@@ -76,11 +64,12 @@ class interpreter:
         while(True):
             packed_op = self.mem[self.pc]
             self.pc += 1
-
+            
             op = packed_op % 100
-            op_bits = int(packed_op / 100)
+            op_bits = packed_op // 100
 
-            instr_modes = tuple(map(lambda x : (op_bits // 10**x) % 10, range(3)))
+            instr_modes = [(op_bits // 10**x) % 10 for x in range(3)]
+            assert all(x in range(3) for x in instr_modes), "Bad instruction mode."
  
             if op == 99:
                 self.halted = True
@@ -90,13 +79,11 @@ class interpreter:
             elif op == 2:
                 self._binary_op(lambda x, y : x * y, instr_modes)
             elif op == 3:
-                p0 = self._get_val(self._read_mem(self.pc), instr_modes[0])
-                self.pc += 1
-                self._write_mem(p0, self._get_input())
+                params = self._get_param_addrs(instr_modes, 1)
+                self._write_mem(params[0], self._get_input())
             elif op == 4:
-                p0 = self._read_mem(self.pc)
-                self.pc += 1
-                self._write_output(self._get_val(p0, instr_modes[0]))
+                params = self._get_param_addrs(instr_modes, 1)
+                self._write_output(self._read_mem(params[0]))
             elif op == 5:
                 self._jump_op(lambda x : x != 0, instr_modes)
             elif op == 6:
@@ -106,9 +93,8 @@ class interpreter:
             elif op == 8:
                 self._binary_op(lambda x, y : 1 if x == y else 0, instr_modes)
             elif op == 9:
-                p0 = self._get_val(self._read_mem(self.pc), instr_modes[0])
-                self.pc += 1
-                self.relative_base += p0 
+                params = self._get_param_addrs(instr_modes, 1)
+                self.relative_base += self._read_mem(params[0]) 
             else:
                 raise ValueError(f"Bad opcode {op}")
 
