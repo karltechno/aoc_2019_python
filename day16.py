b476cea056a34f213e6cfbc9534bbe47afce5ee2
Karl Davis <karl@karltechno.com>
2019-12-26 11:28:22 +0000

    day16 much faster with numpy

--- day16.py
+++ day16.py
@@ -1,24 +1,31 @@
 import itertools
+import numpy as np
 
-def fft_pass(digits):
+def do_fft_passes(digits, num_passes):
+    mtx = []
     base_seq = [0, 1, 0, -1]
-    output = []
     for i in range(len(digits)):
         seq = [y for x in base_seq for y in (x,)*(i+1)]
         iter = itertools.cycle(seq)
         next(iter)
+        mtx.append([x for x in itertools.islice(iter, len(digits))])
 
-        o = sum(x * y for x, y in zip(digits, iter))
-        output.append(abs(o) % 10)
+    mtx = np.array(mtx, dtype=int)
+
+    output = np.array(digits.copy(), dtype=int)
+
+    for _ in range(num_passes):
+        np.dot(mtx, output, out=output)
+        np.abs(output, out=output)
+        np.mod(output, 10, out=output)
 
     return output
+    
 
 if __name__ == "__main__":
     with open("inputs/day16.txt") as f:
         digits = [int(x) for x in f.read()]
 
+    result = do_fft_passes(digits, 100)
 
-    for _ in range(100):
-        digits = fft_pass(digits)
-
-    print("part1: " + "".join(str(x) for x in digits[:8]))
+    print("part1: " + "".join(str(x) for x in result[:8]))
